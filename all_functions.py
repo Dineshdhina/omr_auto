bad6e1f1753cd3e717672b5efef324cb429c897c
Dinesh <dineshdhina@123>
2021-05-16 00:27:01 +0530

    added reorder function

--- all_functions.py
+++ all_functions.py
@@ -4,19 +4,30 @@ import numpy as np
 class all_func():
     def __init__(self):
         pass
-    def stack_images(self, img_array, ori_img):
+    def stack_images(self, img_array, ori_img, dim):
         # images_stacked = cv2.hconcat(img_array)
         # cv2.imshow("All images",images_stacked)
-        imstack = cv2.resize(ori_img,(500,500))
+        if dim == 3:
+            imstack = cv2.resize(ori_img,(500,500))
         
-        for img in img_array:
-            print(img.shape)
-            print(type(img))
-            im = cv2.resize(img,(500,500))
-            print(im.shape)
-            imstack = np.hstack((imstack, im))
-
-        return imstack
+            for img in img_array:
+                print(img.shape)
+                print(type(img))
+                im = cv2.resize(img,(500,500))
+                print(im.shape)
+                imstack = np.hstack((imstack, im))
+            return imstack
+        else:
+            imstack = cv2.resize(ori_img,(500,500))
+        
+            for img in img_array:
+                print(img.shape)
+                print(type(img))
+                im = cv2.resize(img,(500,500))
+                print(im.shape)
+                imstack = np.hstack((imstack, im))
+            return imstack
+        
 
     def rectangle_contour(self,contours):
         rect_cont = []
@@ -24,10 +35,32 @@ class all_func():
             area = cv2.contourArea(contour)   
             if area > 50:
                 peri = cv2.arcLength(contour, True)
-                print(peri)
                 approx = cv2.approxPolyDP(contour, 0.02*peri,True)
                 if len(approx) == 4:
                     rect_cont.append(contour)
+
+        rect_cont = sorted(rect_cont, key = cv2.contourArea , reverse = True)
+
+        return rect_cont
+
+    def getCornerPoints(self, cont):
+        peri = cv2.arcLength(cont, True)
+        approx = cv2.approxPolyDP(cont, 0.02*peri,True)
+        return approx
+
+    def reorder(self, points):
+        points = points.reshape((4,2))
+        pointsnew = np.zeros((4,1,2),np.int32)
+        add = points.sum(1)
+        pointsnew[0] = points[np.argmin(add)] 
+        pointsnew[3] = points[np.argmax(add)] 
+        diff = np.diff(points,axis = 1)
+        pointsnew[1] = points[np.argmin(diff)]
+        pointsnew[2] = points[np.argmax(diff)]
+
+        return pointsnew
+
+
 
                 
             
--- omr_main.py
+++ omr_main.py
@@ -7,27 +7,45 @@ from all_functions import all_func
 
 width = 500
 height = 500
-
+funcs = all_func()
 
 img = cv2.imread("1.jpg")
 imgcontours = img.copy()
-
+bigimgcontours = img.copy()
+# preprocess the images
 imgs = cv2.resize(img,(width,height))
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (5,5),1)
 imgCanny = cv2.Canny(imgBlur, 10, 50)
 
+# find all contours
 contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(imgcontours, contours,-1, (0,255,0), 10)
 image_contours = cv2.resize(imgcontours,(width,height))
 
 
-image_array =[imgBlur, imgCanny]
+# find rectangles
+rect_cont = funcs.rectangle_contour(contours)
+big_contour = funcs.getCornerPoints(rect_cont[0])
+gradePoints = funcs.getCornerPoints(rect_cont[1])
 
-funcs = all_func()
-imstack = funcs.stack_images(image_array,imgGray)
-funcs.rectangle_contour(contours)
+
+
+if big_contour.size != 0 and gradePoints.size !=0:
+    cv2.drawContours(bigimgcontours,big_contour, -1,(0,255,0),40)
+    cv2.drawContours(bigimgcontours,gradePoints, -1,(0,255,0),40)
+
+    biggest_contour = funcs.reorder(big_contour)
+    gradePoints = funcs.reorder(gradePoints)
+
+
+image_array =[imgBlur, imgCanny]
+image_array_3d = [image_contours,bigimgcontours]
+
+imstack = funcs.stack_images(image_array,imgGray,2)
+imstack_3d = funcs.stack_images(image_array_3d,img,3)
+
 
 cv2.imshow("Original",imstack)
-cv2.imshow("contours",image_contours)
+cv2.imshow("contours",imstack_3d)
 cv2.waitKey(0)
